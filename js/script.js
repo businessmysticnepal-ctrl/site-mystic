d77513a4150842722dd294ad548bd802ed46b1a9
businessmysticnepal-ctrl <business.mystic.nepal@gmail.com>
2025-08-24 20:04:49 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-   // JavaScript for mobile menu toggle
         const mobileMenuButton = document.getElementById('mobile-menu-button');
         const mobileMenu = document.getElementById('mobile-menu');
 
